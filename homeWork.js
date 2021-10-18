1b4aa8a420a441708299942abd987599d4f22a82
Sergen Bakır <sbakir09@outlook.com>
2021-10-18 11:43:18 +0300

    homework5

--- homeWork.js
+++ homeWork.js
@@ -6,3 +6,4 @@ function calculateCircleArea(radius){
 }
 
 calculateCircleArea(arguments[0] * 1);
+
---
+++ homeWork5/index.js
@@ -0,0 +1,27 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h2>INDEX Page</h2>");
+  } else if (url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h2>About Page</h2>");
+  } else if (url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/html", });
+    res.write("<h2>Contact Page</h2>");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write("<h2>404 Page Not Found </h2>");
+  }
+
+  res.end();
+});
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`Sunucu port ${port} de başlatıldı.`);
+});
